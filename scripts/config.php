899e5d45a10459e8e052d272c65c31a94c7e8436
Jacob Sanders <j.q.sanders@gmail.com>
2016-06-12 10:34:44 -0400

    initial page get successful

--- scripts/config.php
+++ scripts/config.php
@@ -1,7 +1,7 @@
 #!usr/bin/env php
 
 <?php
-$url = 'http://www.finewineandgoodspirits.com/webapp/wcs/stores/servlet/FindStoreView?storeId=10051&langId=-1&catalogId=10051&pageNum=1&listSize=700&category=&city=&zip_code=&county=All+Stores&storeNO=%'
+$url = 'http://www.finewineandgoodspirits.com/webapp/wcs/stores/servlet/FindStoreView?storeId=10051&langId=-1&catalogId=10051&pageNum=1&listSize=700&category=&city=&zip_code=&county=All+Stores&storeNO=%';
 
 $config['pgsql_db'] = 'php_scraper';
 $config['user'] = 'jacobsanders';
--- scripts/scrape.php
+++ scripts/scrape.php
@@ -3,11 +3,29 @@
 
 <?php
 include('config.php');
-include('object.php')
+include('object.php');
 
 // open url
+function openURL($url) {
+    // initialize curl resource
+    $ch = curl_init();
+    // provide url
+    curl_setopt($ch, CURLOPT_URL, $url);
+    // stringify response
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    // follow any redirects (shouldn't be any, but just in case)
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+    // store response string
+    $response = curl_exec($ch);
+    // close curl resource
+    curl_close($ch);
 
-// get response
+    echo $response;
+    return $response;
+}
+
+openURL($url);
+
 
 // parse response
 

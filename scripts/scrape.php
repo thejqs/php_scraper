eb23ddda976fabf48feb432ccad6be62995fcac5
Jacob Sanders <j.q.sanders@gmail.com>
2016-06-12 13:54:11 -0400

    data acquired and sorted; preparing for object assembly

--- scripts/scrape.php
+++ scripts/scrape.php
@@ -22,7 +22,8 @@ function getHTML($url) {
     return $response;
 }
 
-function cleanHTML($html) {
+// properly format the HTML for XML parsing
+function handleBrokenHTML($html) {
     $old = libxml_use_internal_errors(true);
     $dom = new DOMDocument;
     $dom->loadHTML($html);
@@ -30,24 +31,58 @@ function cleanHTML($html) {
     return $dom;
 }
 
-function getStoreIDs($xml) {
-    return $xml->xpath('//*[@id="main_container"]/div[4]/div/div[2]/span[2]/text()');
-}
-
-// parse response
+// turns our html into a traversable DOM
 function parseHTML($url) {
     $html = getHTML($url);
     // php says this is some broken-ass html
     // attemtping to repair enough to make usable
-    $dom = cleanHTML($html);
-    $xml = simplexml_import_dom($dom);
-    $storeIDs = getStoreIDs($xml);
-    echo trim($storeIDs[0]);
+    $dom = handleBrokenHTML($html);
+    return simplexml_import_dom($dom);
 }
 
-parseHTML($url);
+// collect an array of store ids
+function getStoreIDs($xmlObj) {
+    $storeIDs = $xmlObj->xpath('//*[@id="main_container"]/div[4]/div/div[2]/span[2]/text()');
+    $cleanIDs = array_map(function($x) {return trim($x);}, $storeIDs);
+    return $cleanIDs;
+}
 
-// clean response
+// collect and array of addresses
+function getAddresses($xmlObj) {
+    return $xmlObj->xpath('//*[@id="main_container"]/div[4]/div/div[5]/form/input[3]/@value');
+}
+
+// collect an array of phone numbers
+function getPhone($xmlObj) {
+    return $xmlObj->xpath('//*[@id="main_container"]/div[4]/div/div[5]/form/input[4]/@value');
+}
+
+// collect an array of latitudes
+function getLatitudes($xmlObj) {
+    return $xmlObj->xpath('//*[@id="main_container"]/div[4]/div/div[5]/form/input[2]/@value');
+}
+
+//collect an array of longitudes
+function getLongitudes($xmlObj) {
+    return $xmlObj->xpath('//*[@id="main_container"]/div[4]/div/div[5]/form/input[1]/@value');
+}
+
+function getData($url) {
+    $xml = parseHTML($url);
+    $storeIDs = getStoreIDs($xml);
+    $addresses = getAddresses($xml);
+    $phoneNumbers = getPhone($xml);
+    $latitudes = getLatitudes($xml);
+    $longitudes = getLongitudes($xml);
+    return sortData($storeIDs, $addresses, $phoneNumbers, $latitudes, $longitudes);
+}
+
+function sortData($listOne, $listTwo, $listThree, $listFour, $listFive) {
+    $zipFirst = array_map(null, $listFour, $listFive);
+    return array_map(null, $listOne, $listTwo, $listThree, $zipFirst);
+}
+
+getData($url);
 
 // store cleaned response
 ?>
